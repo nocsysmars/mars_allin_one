90e7cae640ad1bbcc0596264826a6b0319ec9469
王龙生 <longshengwang@users.noreply.github.com>
2021-04-28 16:04:12 +0800

    Add mars new web build shell. (#41)

---
+++ update_new_web.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# company=prod
+# if [[ $1 == "ares" ]]; then
+#   company=$1
+# fi
+
+#BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+echo "==> 1. Download mars web code."
+git clone --branch main https://github.com/TeamNocsys/mars_web_plus
+if [ $? -ne 0 ]; then
+ echo '==== Download Fail ===='
+ exit $?
+fi
+echo ''
+echo "==> 2. Start to build mars web code."
+cd mars_web_plus
+docker run --rm -v "$PWD":/home/node/web  -w /home/node/web node:12 sh -c "npm install -g @angular/cli && npm install && ng build --prod --output-path=public"
+if [ $? -ne 0 ]; then
+ echo '==== Build Fail ===='
+ exit $?
+fi
+echo ''
+echo "==> 3. Replace the public folder"
+cd ..
+
+if [ ! -d "public" ]; then
+  mkdir public
+fi
+rm -rf public/*
+cp -r mars_web_plus/public/* public/
+rm -rf mars_web_plus
+
