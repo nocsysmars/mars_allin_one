b6f70d3bac405278f421b1bb4ac95db8604065c8
Macauley Cheng <macauley_cheng@accton.com>
2022-04-07 16:40:17 +0800

    Update show_mars_tech.sh

--- tools/show_mars_tech.sh
+++ tools/show_mars_tech.sh
@@ -5,11 +5,10 @@ if [ $# -ne 2 ];
     exit
 fi
 
-echo $1
 
 docker logs mars &> docker_logs
 
-curl  -i -XPOST http://$1/mars/useraccount/v1/login --header 'Content-Type: application/json' --header 'Accept: application/json' -d  '{"user_name":"karaf", "password":"karaf"}' > session_temp
+curl  -XPOST http://$1/mars/useraccount/v1/login --header 'Content-Type: application/json' --header 'Accept: application/json' -d  '{"user_name":"karaf", "password":"karaf"}' > session_temp
 
 token=$(cat session_temp |grep MARS_G_SESSION_ID:|cut -d ":"  -f 2|tr -d '[:space:]')
 
@@ -18,22 +17,21 @@ if [[ $token  == "" ]];
     exit
 fi
 
-curl -i  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/v1/version 1> ctrl_version.log
-curl -i  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/v1/system/systemInfo 1> ctrl_hostinfo.log
-curl -i  -H "Cookie: marsGSessionId=$token" -XGET -k https://$1/build.json 1> web_version.log
-curl -i  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/logs/v1/controller/files/karaf_error.log 1> karaf_error.log
-curl -i  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/logs/v1/controller/files/karaf.log 1> karaf.log
+curl  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/v1/version 1> ctrl_version.log
+curl  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/v1/system/systemInfo 1> ctrl_hostinfo.log
+curl  -H "Cookie: marsGSessionId=$token" -XGET -k https://$1/build.json 1> web_version.log
+curl  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/logs/v1/controller/files/karaf_error.log 1> karaf_error.log
+curl  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/logs/v1/controller/files/karaf.log 1> karaf.log
 for i in $(seq 1 5);
 do
-    echo $i
-    curl -i  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/logs/v1/controller/files/karaf.log.$i 1> karaf.log.$i
+    curl -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/logs/v1/controller/files/karaf.log.$i 1> karaf.log.$i
 done
 
 #from elasticsearch
 off_utc=$(($2+8))
 timestamp=$(date -d "$off_utc hours ago" +%Y-%m-%dT%H:%M:%S.0000Z)
 echo $timestamp
-curl -i  -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/logs/v1/controller?start=$timestamp\&number=2000\&match=\&source=/root/onos/apache-karaf-3.0.8/data/log/karaf.log 1> karaf_from_es.log
+curl -H "Cookie: marsGSessionId=$token" -XGET http://$1:8181/mars/utility/logs/v1/controller?start=$timestamp\&number=2000\&match=\&source=/root/onos/apache-karaf-3.0.8/data/log/karaf.log 1> karaf_from_es.log
 
 
 #get devices
