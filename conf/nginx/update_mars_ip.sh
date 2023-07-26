dc7b6ed111b948e48c8c7247d8b0add89beed644
xianfu.zhang <286330850@qq.com>
2023-07-26 14:52:04 +0800

    fix namespace bug

--- conf/nginx/update_mars_ip.sh
+++ conf/nginx/update_mars_ip.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #take mars k8s service dns as mars ip
-sed -i 's/127.0.0.1/mars.default.svc.cluster.local/g' /etc/nginx/nginx.conf
+sed -i 's/127.0.0.1/mars.nocsys-mars.svc.cluster.local/g' /etc/nginx/nginx.conf
 
 service xinetd start
 service filebeat restart
