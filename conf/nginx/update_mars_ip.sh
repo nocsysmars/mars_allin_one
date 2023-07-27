d98373b4f1c886aa230812f2468c905d001fedcc
xianfu.zhang <286330850@qq.com>
2023-07-27 09:29:27 +0800

    webssh and websocket should connect to mars

--- conf/nginx/update_mars_ip.sh
+++ conf/nginx/update_mars_ip.sh
@@ -2,6 +2,8 @@
 
 #take mars k8s service dns as mars ip
 sed -i 's/127.0.0.1/mars.nocsys-mars.svc.cluster.local/g' /etc/nginx/nginx.conf
+sed -i 's/http:\/\/localhost:8443/http:\/\/mars.nocsys-mars.svc.cluster.local:8443/g' /etc/nginx/nginx.conf
+sed -i 's/http:\/\/localhost:3233/http:\/\/mars.nocsys-mars.svc.cluster.local:3233/g' /etc/nginx/nginx.conf
 
 service xinetd start
 service filebeat restart
