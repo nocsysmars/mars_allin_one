d0f3ec7c74f28674a069bcc374de3ba8f06022a2
macauleyCheng <macauley_cheng@accton.com>
2024-08-30 10:58:13 +0800

    fix k8s don't change backen mars ip address

--- conf/nginx/update_mars_ip.sh
+++ conf/nginx/update_mars_ip.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 #take mars k8s service dns as mars ip
-sed -i 's/127.0.0.1/mars.accton-mars.svc.cluster.local/g' /etc/nginx/nginx.conf
-sed -i 's/http:\/\/localhost:8443/http:\/\/mars.accton-mars.svc.cluster.local:8443/g' /etc/nginx/nginx.conf
-sed -i 's/http:\/\/localhost:3233/http:\/\/mars.accton-mars.svc.cluster.local:3233/g' /etc/nginx/nginx.conf
+sed -i 's/10.10.10.5/mars.accton-mars.svc.cluster.local/g' /etc/nginx/nginx.conf
+#sed -i 's/http:\/\/localhost:8443/http:\/\/mars.accton-mars.svc.cluster.local:8443/g' /etc/nginx/nginx.conf
+#sed -i 's/http:\/\/localhost:3233/http:\/\/mars.accton-mars.svc.cluster.local:3233/g' /etc/nginx/nginx.conf
 
 sed -i 's/localhost:5044/logstash.accton-mars.svc.cluster.local:5044/g' /etc/filebeat/filebeat.yml
 
